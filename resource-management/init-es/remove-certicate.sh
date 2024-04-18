efc48d7289077dfa0ddb93fb7cbcafb23a98e332
ooii8929 <ooii8929@gmail.com>
2024-04-18 10:34:57 +0800

    doc: add solutions1

--- resource-management/init-es/remove-certicate.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete secrets elastic-certificates -n kube-system
-kubectl delete secrets elastic-certificate-pem -n kube-system
-kubectl delete secrets elastic-certificate-crt -n kube-system
-kubectl delete secrets elastic-credentials -n kube-system
-kubectl delete secrets kibana -n kube-system
